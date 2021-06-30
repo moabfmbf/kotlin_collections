39deae8fe5a72686127a0eca8e40a427927d12e2
moabfmbf <moabfmbf@gmail.com>
2021-06-30 13:30:52 -0400

    novo commit

--- src/main/kotlin/one/digitallinovation/collections/TestArrayString.kt
+++ src/main/kotlin/one/digitallinovation/collections/TestArrayString.kt
@@ -1,4 +1,32 @@
 package one.digitallinovation.collections
 
-class TestArrayString {
+fun main() {
+    /*
+    val nomes = Array(4) { "" }
+    nomes[0] = "Paula"
+    nomes[1] = "Maria"
+    nomes[2] = "Sandra"
+    nomes[3] = "Adriana"
+
+    for (nome in nomes) {
+        println("Nomes dos usuarios " + nome)
+    }
+
+    nomes.forEach {
+        println("Nomes do usuarios usando IT " + it)
+    }
+    nomes.forEach { nomesUsarios ->
+        println("Nomes usuarios usando lambdas " + nomesUsarios)
+    }
+    nomes.sort()
+    for(nome in nomes){
+        println(nome)
+    }
+     */
+
+    val nomes = arrayOf("Gomes","Lucas","Maria","Sandra","Paula","Ricardo")
+    nomes.sort()
+    for(nome in nomes){
+        println(nome)
+    }
 }
